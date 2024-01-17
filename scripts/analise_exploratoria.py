849219f230b4d89af4e63580bb7866c3bf6d0450
VtSilveira <vitor.silveiravtss@gmail.com>
2024-01-17 17:23:20 -0300

    commit inicial

---
+++ scripts/analise_exploratoria.py
@@ -0,0 +1,14 @@
+# ################################################################
+# PROJETO FINAL
+#
+# Universidade Federal de Sao Carlos (UFSCAR)
+# Departamento de Computacao - Sorocaba (DComp-So)
+# Disciplina: Aprendizado de Maquina
+# Prof. Tiago A. Almeida
+#
+#
+# Aluno:
+# RA:
+# ################################################################
+
+# Arquivo com todas as funcoes e codigos referentes a analise exploratoria
